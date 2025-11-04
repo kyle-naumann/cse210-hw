10a61f115d6a2911224c7413281385fb15dcdc8a
kyle-naumann <knaumann@byupathway.edu>
2025-11-04 13:21:56 -0800

    finished Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            userNumber = int.Parse(userInput);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = numbers.Sum();
+
+        double average = (double)sum / numbers.Count;
+
+        int largest = numbers.Max();
+
+        int smallestPositive = numbers.Where(x => x > 0).Min();
+
+        numbers.Sort();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        Console.WriteLine("The sorted list is:");
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
